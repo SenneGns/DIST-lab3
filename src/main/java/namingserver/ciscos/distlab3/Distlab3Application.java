d4fcb4635736defd87347207797733b99b7b596d
SenneGns <senne.geens2@student.uantwerpen.be>
2026-03-30 17:02:10 +0200

    initial spring boot setup

---
+++ src/main/java/namingserver/ciscos/distlab3/Distlab3Application.java
@@ -0,0 +1,13 @@
+package namingserver.ciscos.distlab3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Distlab3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Distlab3Application.class, args);
+	}
+
+}
---
+++ src/test/java/namingserver/ciscos/distlab3/Distlab3ApplicationTests.java
@@ -0,0 +1,13 @@
+package namingserver.ciscos.distlab3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Distlab3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
