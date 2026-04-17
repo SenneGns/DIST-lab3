ef1906ecfdb9ec4a2be878f901ce5dc58757bcde
Wout <wout.geens@student.uantwerpen.be>
2026-04-17 16:49:42 +0200

    Commentaar gefixt

--- src/main/java/namingserver/ciscos/distlab3/service/HashService.java
+++ src/main/java/namingserver/ciscos/distlab3/service/HashService.java
@@ -4,15 +4,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class HashService {
 
-    private static final long MAX = 2147483647L;
+    private static final long MAX = 2147483647L; // L prevents overflow (because now it gets treated as long in calc)
     private static final long MIN = -2147483647L;
-    private static final int NEW_MAX = 32768;
+    private static final int NEW_MAX = 32768; // Modified max number for hashing
 
     public int hash(String input) {
         long raw = input.hashCode();
 
-        double scaled = (raw + MAX) * ((double) NEW_MAX / (MAX + Math.abs(MIN)));
+        double scaled = (raw + MAX) * ((double) NEW_MAX / (MAX + Math.abs(MIN))); // Now it's between 0 and 32768
 
-        return (int) Math.round(scaled);
+        return (int) Math.round(scaled); // Afronden
     }
 }
