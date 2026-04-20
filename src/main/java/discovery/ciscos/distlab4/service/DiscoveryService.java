e8060d5944e621b2cbc6877e87747b8d0f976698
lucasgrootaert <lucas.grootaert@gmail.com>
2026-04-20 17:06:12 +0200

    last changes lucas

---
+++ src/main/java/discovery/ciscos/distlab4/service/DiscoveryService.java
@@ -0,0 +1,32 @@
+package discovery.ciscos.distlab4.service;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class DiscoveryService {
+
+    public void sendMulticast(String message, String groupAddress, int port) {
+        // Gebruik een try-with-resources om de socket automatisch te sluiten
+        try (DatagramSocket socket = new DatagramSocket()) {
+            InetAddress group = InetAddress.getByName(groupAddress);
+            byte[] buffer = message.getBytes();
+
+            // Maak het pakket aan gericht aan de multicast groep
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length, group, port);
+
+            // Verstuur het bericht
+            socket.send(packet);
+            System.out.println("Multicast bericht verzonden: " + message);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        DiscoveryService ds = new DiscoveryService();
+        // 224.0.0.1 is het standaard adres voor alle systemen op het lokale subnet
+        ds.sendMulticast("DISCOVERY_REQUEST:NodeA", "224.0.0.100", 4567);
+    }
+}
